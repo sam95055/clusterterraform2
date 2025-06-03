8e1db9c990505f889144820078084110dd1c5a03
sam95055 <sam95055@gmail.com>
2025-06-03 15:29:57 +0530

    Update Bars.tf

--- Bars.tf
+++ Bars.tf
@@ -31,3 +31,20 @@ output "cluster_name" {
 output "karpenter_role_arn" {
   value = module.karpenter_irsa.iam_role_arn
 }
+
+
+module "karpenter_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+
+  create_role                   = true
+  role_name                     = "${var.name_prefix}-karpenter"
+  provider_url                  = module.eks.cluster_oidc_issuer_url
+  role_policy_arns              = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  ]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+  tags                          = var.tags
+}
+
