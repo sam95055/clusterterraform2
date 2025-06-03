9bb2d46f8424b0a332a0e4cc1552780887caa6d7
sam95055 <sam95055@gmail.com>
2025-06-03 15:32:48 +0530

    Update Bars.tf

--- Bars.tf
+++ Bars.tf
@@ -48,3 +48,17 @@ module "karpenter_irsa" {
   tags                          = var.tags
 }
 
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
